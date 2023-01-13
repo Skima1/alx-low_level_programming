cf0afddecff76620e27d90e0d61242dd43f3d233
Skima1 <ayodeleoloniyo@gmail.com>
2023-01-13 02:03:20 -0800

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _memset - fills memory with a constant byte
+ *
+ * @s: input pointer that represents memory block
+ *     to fill
+ * @b: characters to fill/set
+ * @n: number of bytes to be filled
+ *
+ * Return: pointer to the filled memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
