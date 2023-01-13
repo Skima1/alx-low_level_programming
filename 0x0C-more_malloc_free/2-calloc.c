0b2e5eaf7f38010f5540f743c55d3cdeb330ce30
Skima1 <ayodeleoloniyo@gmail.com>
2023-01-13 01:51:34 -0800

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+/**
+ * _memset - fills memory with a constant byte
+ *
+ * @s: input pointer that represents memory
+ *     block to fill
+ * @b: characters to fill
+ * @n: number of bytes to be filled
+ *
+ * Return: A pointer to the filled memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
