07ad6755a92caad8bb37f52bb97dff16137c89c1
Skima1 <ayodeleoloniyo@gmail.com>
2023-01-16 04:23:11 -0800

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+nclude <stdlib.h>
+#include "dog.h"
+
+/**
+ * _strlen - a function that gets a length of string
+ *
+ * @str: the string to get the length
+ *
+ * Return: length of @str
+ */
+
+int _strlen(const char *str)
+{
