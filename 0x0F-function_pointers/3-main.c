7baad59885210daa073f2eaec800df2315786f01
Skima1 <ayodeleoloniyo@gmail.com>
2023-01-18 12:18:02 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,4 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <string.h> /*for atoi*/
+#include <stdlib.h> /*for exit*/
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,11 @@
+/**
+ * op_add - adds @a and @b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: sum of @a and @b
+ */
+
+int op_add(int a, int b)
+{
