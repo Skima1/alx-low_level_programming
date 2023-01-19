960e650d8041b619d13ab95448c143917fbecc33
Skima1 <ayodeleoloniyo@gmail.com>
2023-01-19 00:53:02 -0800

    Update

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,8 @@
+#include "variadic_functions.h"
+
+/**
+ *  * print_all - prints anything.
+ *   * @format: a list of types of arguments passed to the function.
+ *    *
+ *     * Return: no return.
+ *      */
