d0642881555c0713043122999be2bbf01fe703cf
Oloniyo Sunday <ayodeleoloniyo@gmail.com>
2023-02-06 10:36:55 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,59 +4,59 @@
 #include "main.h"
 
 /**
- *  * append_text_to_file - apends text to file
- *   * @filename: path to file
- *    * @text_content: content
- *     * Return: 1 or -1
- *      */
+ * append_text_to_file - apends text to file
+ * @filename: path to file
+ * @text_content: content
+ * Return: 1 or -1
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-		int fd;
-			ssize_t w;
-				int size;
+	int fd;
+	ssize_t w;
+	int size;
 
-					if (!filename)
-								return (-1);
+	if (!filename)
+		return (-1);
 
-						fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-							if (fd == -1)
-										return (-1);
+	if (fd == -1)
+		return (-1);
 
-								if (!text_content)
-										{
-													close(fd);
-															return (1);
-																}
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
 
-									size = _strlen(text_content);
-										w = write(fd, text_content, size);
+	size = _strlen(text_content);
+	w = write(fd, text_content, size);
 
-											if (w == -1)
-													{
-																close(fd);
-																		return (-1);
-																			}
-												close(fd);
-													return (1);
+	if (w == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
 }
 
 /**
- *  * _strlen - len
- *   *
- *    * @s: is a pointer to a char
- *     *
- *      * Return: Always 0.
- *       */
+ * _strlen - len
+ *
+ * @s: is a pointer to a char
+ *
+ * Return: Always 0.
+ */
 
 int _strlen(const char *s)
 {
-		int i = 0;
+	int i = 0;
 
-			while (*(s + i) != '\0')
-					{
-								i++;
-									}
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
 
-				return (i);
+	return (i);
 }
